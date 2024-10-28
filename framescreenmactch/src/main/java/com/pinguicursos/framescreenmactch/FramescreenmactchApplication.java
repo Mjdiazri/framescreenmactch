9d9a7a97ab377c18e430bdd395d1f16cd94a9eef
Mjdiazri <citruss2012@hotmail.es>
2024-10-28 08:07:51 -0500

    Se arregla ObjectMapper importando Jackson jar.Se termina de consumir api y convertir json a clase datoSerie. Se utiliza @Jsonignore para ignorar datos no requeridos y se inicia contruccion del mismo codigo para mostrar datos de un episodio

--- framescreenmactch/src/main/java/com/pinguicursos/framescreenmactch/FramescreenmactchApplication.java
+++ framescreenmactch/src/main/java/com/pinguicursos/framescreenmactch/FramescreenmactchApplication.java
@@ -1,6 +1,9 @@
 package com.pinguicursos.framescreenmactch;
 
+import com.pinguicursos.framescreenmactch.model.DatosEpisodio;
+import com.pinguicursos.framescreenmactch.model.DatosSerie;
 import com.pinguicursos.framescreenmactch.service.ConsumoAPI;
+import com.pinguicursos.framescreenmactch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,5 +20,13 @@ public class FramescreenmactchApplication implements CommandLineRunner {
 		var consumoAPI = new ConsumoAPI();
 		var json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?apikey=64058bad&t=game+of+thrones");
 		System.out.println(json);
+
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
+
+		json = consumoAPI.obtenerDatos("https://www.omdbapi.com/?apikey=64058bad&t=game+of+thrones&Season=1&Episode=1");
+		var episodio = conversor.obtenerDatos(json, DatosEpisodio.class);
+		System.out.println(episodio);
 	}
 }
--- framescreenmactch/src/main/java/com/pinguicursos/framescreenmactch/service/ConvierteDatos.java
+++ framescreenmactch/src/main/java/com/pinguicursos/framescreenmactch/service/ConvierteDatos.java
@@ -1,6 +1,19 @@
 package com.pinguicursos.framescreenmactch.service;
 
-public class ConvierteDatos {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pinguicursos.framescreenmactch.model.DatosSerie;
+
+public class ConvierteDatos implements IConvierteDatos{
+   private ObjectMapper objectMapper = new ObjectMapper();
 
 
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return objectMapper.readValue(json, clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
